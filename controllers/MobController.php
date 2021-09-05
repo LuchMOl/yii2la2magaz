da5c758f318e55706c9e2cbe898ffc5bec1ffa77
LuchMOl <hawktm@ukr.net>
2021-09-05 22:58:24 +0300

    filter begin

--- controllers/MobController.php
+++ controllers/MobController.php
@@ -4,45 +4,38 @@ namespace app\controllers;
 
 use app\models\Mob;
 use app\models\MobParam;
+use app\models\Skill;
 use app\models\MobSkill;
 use app\models\MobDrop;
 use app\models\MobSweep;
+use app\models\Param;
 use yii\helpers\Url;
 use yii\web\YiiAsset;
+use yii\helpers\ArrayHelper;
 
 class MobController extends \yii\web\Controller
 {
 
     public function actionIndex()
     {
-        $quantityItemInPage = 20;
+        //var_dump($_GET['Типы-атаки']);die;
+        $quantityItemInPage = 30;
 
-        $mobCount = Mob::find()
-                ->count();
+        $quantityPages = $this->getQuantityPages($quantityItemInPage);
 
-        $quantityPages = ceil($mobCount / $quantityItemInPage);
+        $currentPageNumber = $this->checkPagesRange($quantityPages);
 
-        if (isset($_GET['page'])) {
-            $currentPageGet = $_GET['page'];
+        $attackTypeList = $this->getParamList($this->getParamId('Тип атаки'));
 
-            if ($currentPageGet < 1) {
-                $currentPageGet = 1;
-                $this->redirect('/mob/?page=1');
-            } elseif ($currentPageGet > $quantityPages) {
-                $currentPageGet = $quantityPages;
-                $this->redirect('/mob/?page=' . $quantityPages);
-            }
-        } else {
-            $currentPageGet = 1;
-            $this->redirect('/mob/?page=1');
-        }
+        $raceList = $this->getRaceList();
 
-        $mobsForPage = Mob::find()
-                ->offset($quantityItemInPage * ($currentPageGet - 1))
-                ->limit($quantityItemInPage)
-                ->all();
+        $mobsForPage = $this->getMobsForPage($quantityItemInPage, $currentPageNumber);
 
-        return $this->render('index', compact('mobsForPage', 'currentPageGet', 'quantityPages'));
+        return $this->render('index', compact('mobsForPage',
+                                'currentPageNumber',
+                                'quantityPages',
+                                'attackTypeList',
+                                'raceList'));
     }
 
     public function actionView()
@@ -70,6 +63,117 @@ class MobController extends \yii\web\Controller
         }
 
         return $this->render('singleMob', compact('mob', 'mobParam', 'mobSkill', 'mobDrop', 'mobSweep'));
+    }
+
+    public function getQuantityPages($quantityItemInPage)
+    {
+        $mobCount = Mob::find()
+                ->count();
+
+        $quantityPages = ceil($mobCount / $quantityItemInPage);
+        return $quantityPages;
+    }
+
+    public function checkPagesRange($quantityPages)
+    {
+        if (isset($_GET['pageNumber'])) {
+
+            $currentPageNumber = $_GET['pageNumber'];
+
+            if ($currentPageNumber < 1) {
+                $currentPageNumber = 1;
+                $this->redirect('/mob/?pageNumber=1');
+            } elseif ($currentPageNumber > $quantityPages) {
+                $currentPageNumber = $quantityPages;
+                $this->redirect('/mob/?pageNumber=' . $quantityPages);
+            }
+        } else {
+            $currentPageNumber = 1;
+            $this->redirect('/mob/?pageNumber=1');
+        }
+        return $currentPageNumber;
+    }
+
+    public function getMobsForPage($quantityItemInPage, $currentPageGet)
+    {
+        if (isset($_GET['attackType'])) {
+
+            $mobsIdWithAttackType = MobParam::find()
+                    ->where(['paramValue' => $_GET['attackType']])
+                    ->asArray()
+                    ->all();
+
+            $mobsIdWithAttackType = ArrayHelper::getColumn($mobsIdWithAttackType, 'mobId');
+
+            $mobsForPage = Mob::find()
+                    ->where(['id' => $mobsIdWithAttackType])
+                    ->offset($quantityItemInPage * ($currentPageGet - 1))
+                    ->limit($quantityItemInPage)
+                    ->all();
+        } elseif ($_GET['race']) {
+            $skillId = $this->getSkillIdWhere($title = 'Race', $description = $_GET['race']);
+
+            $mobsIdWithRace = MobSkill::find()
+                    ->select('mobId')
+                    ->where(['skillId' => $skillId])
+                    ->asArray()
+                    ->all();
+            //var_dump($mobsIdWithRace);die;
+
+            $mobsIdWithRace = ArrayHelper::getColumn($mobsIdWithRace, 'mobId');
+
+            $mobsForPage = Mob::find()
+                    ->where(['id' => $mobsIdWithRace])
+                    ->offset($quantityItemInPage * ($currentPageGet - 1))
+                    ->limit($quantityItemInPage)
+                    ->all();
+        }
+        return $mobsForPage;
+    }
+
+    public function getParamId($paramName)
+    {
+        $paramId = Param::find()
+                ->where(['paramName' => $paramName])
+                ->one();
+        return $paramId->id;
+    }
+
+    public function getParamList($paramId)
+    {
+        $paramList = MobParam::find()
+                ->select('paramValue')
+                ->distinct()
+                ->where(['paramId' => $paramId])
+                ->asArray()
+                ->all();
+        $paramList = ArrayHelper::getColumn($paramList, 'paramValue');
+
+        return $paramList;
+    }
+
+    public function getRaceList()
+    {
+        $raceList = Skill::find()
+                ->select('description')
+                ->distinct()
+                ->where(['title' => 'Race'])
+                ->asArray()
+                ->all();
+        $raceList = ArrayHelper::getColumn($raceList, 'description');
+
+        return $raceList;
+    }
+
+    public function getSkillIdWhere($title, $description)
+    {
+        $skillId = Skill::find()
+                ->select('id')
+                ->andWhere(['title' => $title])
+                ->andWhere(['description' => $description])
+                ->one();
+        //var_dump($skillId);die;
+        return $skillId;
     }
 
 }
--- views/mob/index.php
+++ views/mob/index.php
@@ -2,56 +2,115 @@
 /* @var $this yii\web\View */
 
 define("MOB_IMAGE_PATH", "/i/l2db/mob/");
-define("COUNT_OF_ROW", 4);
+define("COUNT_OF_ROW", 5);
 $count = 0;
-//var_dump($currentPageGet);die;
+//var_dump($listsForSidebarFilters);die;
 ?>
-<div class="row justify-content-center">
-    <?php if ($currentPageGet > 1): ?>
-        <a href="?page=1"> <button type="button" class="btn btn-info"><<</button> </a>
-        <a href="?page=<?= $currentPageGet - 1; ?>"> <button type="button" class="btn btn-info"><</button> </a>
-    <?php endif; ?>
 
-    <button type="button" class="btn btn-info">...</button>
-
-    <?php if ($currentPageGet < $quantityPages): ?>
-        <a href="?page=<?= $currentPageGet + 1; ?>"> <button type="button" class="btn btn-info"><?= $currentPageGet + 1; ?></button> </a>
-
-        <?php if ($currentPageGet < $quantityPages - 1): ?>
-            <a href="?page=<?= $currentPageGet + 2; ?>"> <button type="button" class="btn btn-info"><?= $currentPageGet + 2; ?></button> </a>
-        <?php endif; ?>
-
-        <a href="?page=<?= $currentPageGet + 1; ?>"> <button type="button" class="btn btn-info">></button> </a>
-        <a href="?page=<?= $quantityPages; ?>"> <button type="button" class="btn btn-info">>></button> </a>
-    <?php endif; ?>
-</div>
-
-<?php foreach ($mobsForPage as $mob) : ?>
-
-    <?php if ($count % COUNT_OF_ROW == 0): ?><div class="row justify-content-center"><?php endif; ?>
-        <a href="view/?id=<?= $mob->id ?>">
-            <div class="col-<?= intdiv(12, COUNT_OF_ROW); ?> mob">
-
-                <div class="row">
-
-                    <?php if ($mob->imageFileName !== ''): ?>
-                        <img class="img-thumbnail" src="<?= MOB_IMAGE_PATH . $mob->imageFileName ?>">
-                    <?php else : ?>
-                        <img class="img-thumbnail" src="<?= MOB_IMAGE_PATH . 'noImage.jpg' ?>">
-                    <?php endif; ?>
-
-                </div>
-
-                <div class="row" >
-                    <p class="mob-title"><?= $mob->title ?></p>
-                </div>
-        </a>
+<div class="sidebar-container">
+    <div class="sidebar-logo">
+        Sidebar
     </div>
 
-    <?php if ($count % COUNT_OF_ROW == COUNT_OF_ROW - 1): ?></div><?php endif; ?>
+    <ul class="sidebar-navigation">
+        <li class="header">Тип атаки</li>
+        <?php foreach ($attackTypeList as $item) : ?>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" <?= $item == $_GET['attackType'] ? 'checked' : ''; ?>>
+                <label class="form-check-label" for="flexCheckDefault">
+                    <li><a href="?pageNumber=<?= $currentPageNumber; ?>&attackType=<?= $item; ?>"><?= $item; ?></a></li>
+                </label>
+            </div>
+        <?php endforeach; ?>
+    </ul>
 
-    <?php $count++; ?>
+    <ul class="sidebar-navigation">
+        <li class="header">Рассы</li>
+        <?php foreach ($raceList as $item) : ?>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" <?= $item == $_GET['race'] ? 'checked' : ''; ?>>
+                <label class="form-check-label" for="flexCheckDefault">
+                    <li><a href="?pageNumber=<?= $currentPageNumber; ?>&race=<?= $item; ?>"><?= $item; ?></a></li>
+                </label>
+            </div>
+        <?php endforeach; ?>
+    </ul>
 
-<?php endforeach; ?>
+    <ul class="sidebar-navigation">
+        <li class="header">С фото без фото</li>
+        <li>
+            <a href="#">
+                <i class="fa fa-users" aria-hidden="true"></i> Friends
+            </a>
+        </li>
+    </ul>
 
+    <ul class="sidebar-navigation">
+        <li class="header">по уровню, слайдером</li>
+        <li>
+            <a href="#">
+                <i class="fa fa-users" aria-hidden="true"></i> Friends
+            </a>
+        </li>
+    </ul>
 
+</div>
+
+<div class="content-container">
+
+    <div class="container-fluid">
+
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="jumbotron">
+
+            <!-- BUTTONS -->
+            <div class="row justify-content-center">
+                <?php if ($currentPageNumber > 1): ?>
+                    <a href="?pageNumber=1"> <button type="button" class="btn btn-info"><<</button> </a>
+                    <a href="?pageNumber=<?= $currentPageNumber - 1; ?>"> <button type="button" class="btn btn-info"><</button> </a>
+                <?php endif; ?>
+
+                <button type="button" class="btn btn-info">...</button>
+
+                <?php if ($currentPageNumber < $quantityPages): ?>
+                    <a href="?pageNumber=<?= $currentPageNumber + 1; ?>"> <button type="button" class="btn btn-info"><?= $currentPageNumber + 1; ?></button> </a>
+
+                    <?php if ($currentPageNumber < $quantityPages - 1): ?>
+                        <a href="?pageNumber=<?= $currentPageNumber + 2; ?>"> <button type="button" class="btn btn-info"><?= $currentPageNumber + 2; ?></button> </a>
+                    <?php endif; ?>
+
+                    <a href="?pageNumber=<?= $currentPageNumber + 1; ?>"> <button type="button" class="btn btn-info">></button> </a>
+                    <a href="?pageNumber=<?= $quantityPages; ?>"> <button type="button" class="btn btn-info">>></button> </a>
+                <?php endif; ?>
+            </div>
+
+            <!-- MOBS TILE -->
+            <?php foreach ($mobsForPage as $mob) : ?>
+
+                <?php if ($count % COUNT_OF_ROW == 0): ?><div class="row justify-content-center"><?php endif; ?>
+                    <a href="view/?id=<?= $mob->id ?>">
+                        <div class="col-<?= intdiv(12, COUNT_OF_ROW); ?> mob">
+
+                            <div class="row">
+                                <?php if ($mob->imageFileName !== ''): ?>
+                                    <img class="img-thumbnail" src="<?= MOB_IMAGE_PATH . $mob->imageFileName ?>">
+                                <?php else : ?>
+                                    <img class="img-thumbnail" src="<?= MOB_IMAGE_PATH . 'noImage.jpg' ?>">
+                                <?php endif; ?>
+                            </div>
+
+                            <div class="row" >
+                                <p class="mob-title"><?= $mob->title ?></p>
+                            </div>
+                    </a>
+                </div>
+
+                <?php if ($count % COUNT_OF_ROW == COUNT_OF_ROW - 1): ?></div><?php endif; ?>
+
+            <?php $count++; ?>
+
+        <?php endforeach; ?>
+
+    </div>
+</div>
+</div>
